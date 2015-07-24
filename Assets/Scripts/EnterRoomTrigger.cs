c0326d36ca91e0a3568a732c1cd20bf237c32234
Jacek Przemieniecki <jacek@przemieniecki.net>
2015-07-24 22:40:05 +0200

    Zamykanie drzwi

--- Assets/Scripts/EnterRoomTrigger.cs
+++ Assets/Scripts/EnterRoomTrigger.cs
@@ -1,7 +1,21 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
-public class EnterRoomTrigger : MonoBehaviour 
+public class EnterRoomTrigger : MonoBehaviour
 {
-    
+    public Transform CamPosition;
+    public GameObject Door;
+    public GameObject NoReturnCollider;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        CameraControler.MoveTo(CamPosition.position);
+        NoReturnCollider.SetActive(true);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        GetComponent<Collider2D>().isTrigger = false;
+        Door.gameObject.SetActive(true);
+    }
 }

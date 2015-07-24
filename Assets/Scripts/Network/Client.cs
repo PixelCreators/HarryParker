b5e301db7107eaed5238295714c03db9b967f7f7
Mateusz Pusty <mateusz.pusty@pixelcreators.pl>
2015-07-24 22:45:07 +0200

    Working server - client over PC - Windows Phone

---
+++ Assets/Scripts/Network/Client.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Client : MonoBehaviour
+{
+    NetworkClient _networkClient;
+    public InputField IPInputField;
+    
+
+    void Start ()
+    {
+        _networkClient = new NetworkClient();
+	}
+
+    public void Connect()
+    {
+        ///sdf
+
+        DebugConsole.Instance.PrintString("DupaDebug");
+        var ip = IPInputField.text;
+        _networkClient.Connect(ip, 1337);
+        _networkClient.RegisterHandler(MsgType.Connect, OnConnected);
+        _networkClient.RegisterHandler(MsgType.Error, OnError);
+    }
+
+    public void OnConnected(NetworkMessage netMsg)
+    {
+        DebugConsole.Instance.PrintString("Connected to server");
+    }
+
+    public void OnError(NetworkMessage netMsg)
+    {
+        DebugConsole.Instance.PrintString("Error");
+    }
+	
+	void Update ()
+    {
+	
+	}
+}
---
+++ Assets/Scripts/Network/Server.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Server : MonoBehaviour
+{
+
+    void Start()
+    {
+        NetworkServer.Listen(1337);
+
+        if(NetworkServer.active)
+            DebugConsole.Instance.PrintString("Server started");
+
+    }
+}

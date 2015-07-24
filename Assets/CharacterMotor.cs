13cdd177f9e683d35189dd406f41d8e49a7d2235
Jacek Przemieniecki <jacek@przemieniecki.net>
2015-07-24 19:03:32 +0200

    Character motor

---
+++ Assets/CharacterMotor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof (Rigidbody2D))]
+public class CharacterMotor : MonoBehaviour
+{
+    public float Speed;
+    private Rigidbody2D _rigidbody;
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        var direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        _rigidbody.velocity = direction*Speed;
+    }
+}

607260d4b1a1a6509a71c8464941ad7d617b8863
Jacek Przemieniecki <jacek@przemieniecki.net>
2015-07-24 22:02:25 +0200

    room and decision ui prototypes

---
+++ Assets/Scripts/DecisionButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DecisionButton : MonoBehaviour
+{
+    public Text ButtonText;
+    private string decision;
+
+    public void Init(string decision)
+    {
+        ButtonText.text = decision;
+        this.decision = decision;
+    }
+
+    public void OnClick()
+    {
+        DecisionDisplay.ChooseDecision(decision);
+    }
+}
---
+++ Assets/Scripts/TestRunner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+public class TestRunner : MonoBehaviour 
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Run();
+        }
+    }
+
+    public Decision testDecision;
+
+    void Run()
+    {
+        DecisionDisplay.TriggerDecision(testDecision);
+    }
+}

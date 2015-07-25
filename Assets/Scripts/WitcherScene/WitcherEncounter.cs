fdf23625aeca69f860c0567f9f5d2f5ad2d1e03e
Jacek Przemieniecki <jacek@przemieniecki.net>
2015-07-25 23:40:00 +0200

    placeholders

---
+++ Assets/Scripts/WitcherScene/WitcherEncounter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+public class WitcherEncounter : MonoBehaviour
+{
+    private WitcherEncounter _instance;
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (_instance == this)
+        {
+            _instance = null;
+        }
+    }
+}

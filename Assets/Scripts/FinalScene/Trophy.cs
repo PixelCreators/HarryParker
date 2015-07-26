0ea006d611b5050446d77e5361801d63a2799bb1
Jacek Przemieniecki <jacek@przemieniecki.net>
2015-07-26 12:54:18 +0200

    wieśmin 2

---
+++ Assets/Scripts/FinalScene/Trophy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+public class Trophy : MonoBehaviour
+{
+    public Transform Hulk;
+    public GameObject ChargingHulk;
+    public GameObject SmashingHulk;
+    public float RushTime = 2.5f;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Hulk.gameObject.SetActive(true);
+        }
+    }
+
+    IEnumerator HulkCoroutine()
+    {
+        float progress = 0;
+        float start = Time.time;
+        Vector3 startPosition = Hulk.position;
+
+        while (progress < 1)
+        {
+            progress = (start - Time.time)/RushTime;
+            Hulk.position = Vector3.Lerp(startPosition, PlayerPosition.PlayerTransform.position, progress);
+            yield return null;
+        }
+        
+        ChargingHulk.SetActive(false);
+        SmashingHulk.SetActive(true);
+        Player.Kill();
+    }
+}
